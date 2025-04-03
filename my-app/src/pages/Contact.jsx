ba30eb01b0abdd0097a04105960b7042dfc082d7
unknown <kriyonainfotech@gmail.com>
2025-04-03 20:18:24 +0530

    save

--- my-app/src/pages/Contact.jsx
+++ my-app/src/pages/Contact.jsx
@@ -15,7 +15,7 @@ const ContactForm = () => {
   const onSubmit = async (data) => {
     setIsSubmitting(true);
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("https://kriyonastudio-backend.vercel.app/send-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
