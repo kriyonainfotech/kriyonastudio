8abd092baddbdff24c1f127b79ad5ca6b0c22d2c
unknown <kriyonainfotech@gmail.com>
2025-04-04 13:32:37 +0530

    save

--- backend/index.js
+++ backend/index.js
@@ -108,7 +108,7 @@ app.post("/send-email", async (req, res) => {
       .status(200)
       .json({ success: true, message: "Email sent successfully!" });
   } catch (error) {
-    console.error("Error sending email:", error);
+    console.log("Error sending email:", error);
     res.status(500).json({ success: false, message: "Email failed to send." });
   }
 });
